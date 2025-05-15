b5065aa06bb021e6c87d49f6b8bc76e472e9b4f9
Droid Universall <zzautop@laposte.net>
2025-05-15 16:47:38 +0200

    Streamlit second commit

--- app.py
+++ app.py
@@ -9,18 +9,19 @@ import streamlit as st
 
 st.title("Data Visualization web ap")
 
-st.text('Iris dataset')
+st.text('1 Iris dataset')
+
+df = px.data.iris()
+
+
+st.dataframe(df)
+' 2 Iris dataset'
+
 
 df = px.data.iris()
 
 st.dataframe(df)
-'Iris dataset'
-
-
-df = px.data.iris()
-
-st.dataframe(df)
-st.text('Iris dataset')
+st.text('3 Iris dataset')
 
 fig = px.scatter(df, x='sepal_width',
 y='sepal_length',
@@ -30,3 +31,6 @@ hover_data=['petal_width'],
 marginal_x='box')
 
 st.plotly_chart(fig)
+
+fig = px.scatter(df, x="sepal_width", y="sepal_length")
+fig.show()
